af25a34213ec73c28f39fce19f332181806dc3a0
jumpering <jumpering@gmail.com>
2021-06-01 19:37:49 +0200

    first test

--- src/test/java/usantatecla/mastermind/models/ProposedCombinationTest.java
+++ src/test/java/usantatecla/mastermind/models/ProposedCombinationTest.java
@@ -1,4 +1,12 @@
 package usantatecla.mastermind.models;
 
+import org.junit.Test;
+
 public class ProposedCombinationTest {
+
+    //given
+    @Test
+    public void givenProposedCombinationWhenWrongCombinationThenError(){
+
+    }
 }
